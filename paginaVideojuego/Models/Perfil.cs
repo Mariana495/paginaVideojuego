8cdbbc26d60acb82873b6395a0a7fed2041c6190
José Andrés <andreslozano00@hotmail.com>
2022-06-16 11:10:05 -0500

    Ahora si, modelos de Perfil y UsuarioMJ creados

---
+++ paginaVideojuego/Models/Perfil.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace paginaVideojuego.Models
+{
+    [Keyless]
+    public class Perfil
+    {
+        public UsuarioMJ UsuarioRegistrado { get; set; }
+        public List<Partida> PartidasJugadas { get; set;}
+    }
+}
---
+++ paginaVideojuego/Models/UsuarioMJ.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace paginaVideojuego.Models
+{
+    [Keyless]
+    public class UsuarioMJ
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string ClaveUsuario { get; set; }
+        public DateTime? FechaIngresoUsuario { get; set; }
+        public string ContinenteUsuario { get; set; }
+        public double MinutosJugados { get; set; }
+    }
+}
